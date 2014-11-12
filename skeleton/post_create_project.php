e9b43dd1f89f7d3b2a547a3df68cabe3d9933e30
paul de wouters <paul@hmn.md>
2014-11-12 08:28:23 -0300

    Initial commit

---
+++ skeleton/post_create_project.php
@@ -0,0 +1,9 @@
+#!/usr/bin/php
+<?php
+
+// @TODO: add wp-config with placeholders
+// @TODO: mysql db creation?
+// @TODO: add gruntfile / package.json
+// @TODO: prompt for additional plugins during setup
+
+ 
